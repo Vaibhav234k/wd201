8e98596e5d2973366eb714702f2b11ae4e193876
Vaibhav234k <sonivaibhav378@gmail.com>
2023-03-14 00:51:19 +0530

    Add pre-commit hook

---
+++ todo-cli/todo.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+  const overdue = () => {
+    return all.filter((item) => item.dueDate < today);
+  };
+  const dueToday = () => {
+    return all.filter((item) => item.dueDate == today);
+  };
+  const dueLater = () => {
+    return all.filter((item) => item.dueDate > today);
+  };
+  const toDisplayableList = (list) => {
+    return list.map((item) => `${item.completed ? '[x]':'[ ]'} ${item.title} ${item.dueDate == today ? '' : item.dueDate}`).join('\n');
+  };
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = todoList;
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
+// const todos = todoList();
+
+const formattedDate = (d) => {
+  return d.toISOString().split('T')[0];
+};
+
+const dateToday = new Date();
+const today = formattedDate(dateToday);
