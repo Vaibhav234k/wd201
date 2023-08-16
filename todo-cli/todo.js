ed30e74a7959c65d8d65a76dde72eefe85ffb2f8
Vaibhav234k <sonivaibhav378@gmail.com>
2023-08-16 20:15:53 +0530

    overdue and dueLater updated

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -8,7 +8,7 @@ const todoList = () => {
   };
 
   const overdue = () => {
-    return all.filter((item) => item.dueDate > today);
+    return all.filter((item) => item.dueDate < today);
   };
 
   const dueToday = () => {
@@ -16,7 +16,7 @@ const todoList = () => {
   };
 
   const dueLater = () => {
-    return all.filter((item) => item.dueDate < today);
+    return all.filter((item) => item.dueDate > today);
   };
 
   const toDisplayableList = (list) => {
